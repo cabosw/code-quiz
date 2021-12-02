4a2961d5a5841ea423f2f5e347e5def5e51cb86a
Abigail Oswald <abigailoswald@Abigails-MacBook-Pro.local>
2021-12-02 01:50:27 -0800

    add question content & revealFunction

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,40 @@
-var countdownEl = document.getElementById("timer");
 
-  document.querySelector('#start-quiz').addEventListener('click',function() {
-  var timeLeft = 300;
 
-  var timeInterval = setInterval(function() {
-    document.getElementById("initial").style.visibility='hidden';
-    document.getElementById("high-score").style.visibility='hidden';
-    if (timeLeft > 0){
+//countdown timer function
+function timer() {
+	var countdownEl = document.getElementById("timer");
+	
+	//hides quiz start page content
+	document.getElementById("initial").style.display='none';
+	document.getElementById("high-score").style.display='none';
+	
+	//defines initial start time for countdown
+	var timeLeft = 300;
+	
+	//countdown function
+	var timeInterval = setInterval(function() {
+		
+  //adds timer
+	if (timeLeft > 0){
     countdownEl.textContent= 'Time left: ' + timeLeft ;
     timeLeft--;
     }else{
       clearInterval(timeInterval);
-      countdownEl.textContent ='Time:';
+      countdownEl.textContent ='Time is up';
     }
-  },1000); 
-  console.log(timeInterval);
+  },1000);   
+}
+
+//function to reveal hidden question content
+function revealHidden() {
+	for (let element of document.getElementsByClassName("hidden")){
+		element.style.display="block";
+ }
+}
+
+//start quiz button 
+document.querySelector('#start-quiz').addEventListener('click',function() {
+	revealHidden();
+	timer();
 });
 
